d3eef781a28ed1ed3603e96275d0ef376d70e255
Jamie Williams <jamwil1226@yahoo.com>
2021-11-06 19:16:26 -0400

    add CRUD routes

--- db/schema.sql
+++ db/schema.sql
@@ -1,20 +1,20 @@
-DROP TABLE IF EXISTS department;
-DROP TABLE IF EXISTS role;
-DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employees;
 
-CREATE TABLE department (
+CREATE TABLE departments (
     id INT PRIMARY KEY,
     dept_name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id INT PRIMARY KEY,
     job_title VARCHAR(30),
     salary DECIMAL,
     dept_id INT
 );
 
-CREATE TABLE employee (
+CREATE TABLE employees (
     id INT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,11 @@
-INSERT INTO department (id, dept_name)
+INSERT INTO departments (id, dept_name)
 VALUES
 ('1', 'sales'),
 ('2', 'engineering'),
 ('3', 'finance'),
 ('4', 'legal');
 
-INSERT INTO role (id, job_title, salary, dept_id)
+INSERT INTO roles (id, job_title, salary, dept_id)
 VALUES
 ('1', 'Sales Lead', '100000', '1'),
 ('2', 'Salesperson', '80000', '1'),
@@ -15,7 +15,7 @@ VALUES
 ('6', 'Legal Team Lead', '250000', '4'),
 ('7', 'Lawyer', '190000', '4');
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
 VALUES
 ('1', 'John', 'Doe', '1', '3'),
 ('2', 'Mike', 'Chan', '2', '1'),
