158ec6963038cab7b57357443c95073c00533243
Jamie Williams <jamwil1226@yahoo.com>
2021-11-06 21:02:41 -0400

    put request

--- server.js
+++ server.js
@@ -120,6 +120,85 @@ app.post('/api/employees', ({ body }, res) => {
     });
   });
 
+  // Update a employee's role
+app.put('/api/employee/:id', (req, res) => {
+    const sql = `UPDATE employees SET roles_id = ? 
+                 WHERE id = ?`;
+
+    const errors = inputCheck(req.body, 'roles_id');
+
+    if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+    }
+    const params = [req.body.roles_id, req.params.id];
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        // check if a record was found
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Employee not found'
+        });
+      } else {
+        res.json({
+          message: 'success',
+          data: req.body,
+          changes: result.affectedRows
+        });
+      }
+    });
+  });
+
+  app.get('/api/roles', (req, res) => {
+    const sql = `SELECT * FROM roles`;
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+  app.get('/api/roles/:id', (req, res) => {
+    const sql = `SELECT * FROM roles WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+});
+
+app.delete('/api/roles/:id', (req, res) => {
+    const sql = `DELETE FROM roles WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: res.message });
+        // checks if anything was deleted
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Role not found'
+        });
+      } else {
+        res.json({
+          message: 'deleted',
+          changes: result.affectedRows,
+          id: req.params.id
+        });
+      }
+    });
+  });
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
