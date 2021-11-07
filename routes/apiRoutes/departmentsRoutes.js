2bc41208d1deecf4c21a9e6603a9502c6f2fbabb
Jamie Williams <jamwil1226@yahoo.com>
2021-11-06 21:36:13 -0400

    department get post put delete

---
+++ routes/apiRoutes/departmentsRoutes.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../db/connection');
+const inputCheck = require('../../utils/inputCheck');
+
+router.get('/departments', (req, res) => {
+    const sql = `SELECT * FROM departments`;
+  
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows,
+      });
+    });
+});
+
+// Get single department
+router.get('/department/:id', (req, res) => {
+    const sql = `SELECT * FROM departments WHERE id = ?`;
+    const params = [req.params.id];
+  
+    db.query(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
+
+router.post('/departments', ({ body }, res) => {
+// Data validation
+const errors = inputCheck(body, 'id', 'dept_name');
+if (errors) {
+  res.status(400).json({ error: errors });
+  return;
+}
+const sql = `INSERT INTO departments (id, dept_name) VALUES (?,?)`;
+const params = [body.id, body.dept_name];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+    res.status(400).json({ error: err.message });
+    return;
+    }
+    res.json({
+    message: 'success',
+    data: body
+    });
+});
+});
+
+router.delete('/department/:id', (req, res) => {
+    const sql = `DELETE FROM departments WHERE id = ?`;
+  
+    db.query(sql, req.params.id, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: res.message });
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Department not found'
+        });
+      } else {
+        res.json({
+          message: 'deleted',
+          changes: result.affectedRows,
+          id: req.params.id
+        });
+      }
+    });
+  });
+
+module.exports = router;
--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.use(require('./employeesRoutes'));
 router.use(require('./rolesRoutes'));
+router.use(require('./departmentsRoutes'));
 
 module.exports = router;
